74072eb2db14875d80a455f1e9b07fab8c494fc0
tomershoham10 <tomershoham10@gmail.com>
2025-02-11 11:07:48 +0200

    refactor: renaming component file from page to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import StepIcon from '@/components/StepIcon/page';
+import StepIcon from '@/components/StepIcon/index.tsx';
 
 import { TbArrowsMinimize } from 'react-icons/tb';
 
